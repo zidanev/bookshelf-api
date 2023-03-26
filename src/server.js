b681ffc633290f37549296b27b54e42f4eb3416e
zidanev <zvigamada96@gmail.com>
2023-03-26 19:49:13 +0700

    deploy

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
